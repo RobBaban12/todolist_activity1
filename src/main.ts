615f107bd8f79f5da0a3a8c05d3154d478e85c1d
RobBaban12 <robertllyod.baban-23@cpu.edu.ph>
2024-09-03 13:44:33 +0800

    add task checker

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,27 @@ addTaskButton.addEventListener('click', () => {
     if (task !== '') {
       const li = document.createElement('li');
       const btn = document.createElement('img');
+      const check_btn = document.createElement('img')
+      check_btn.src = "public/checkbox.png";
+      check_btn.id = "check-box";
+
       li.id = "li"
+
       btn.src = 'public/bin.svg';
       btn.id = 'bin';
+
       li.textContent = task;
-    
+
+      taskList.appendChild(check_btn)
       taskList.appendChild(li);
       taskList.appendChild(btn);
 
-      btn.onclick = function(){
+      check_btn.onclick = () => {
+        li.style.textDecoration = li.style.textDecoration === 'line-through' ? 
+        'none' : 'line-through';
+      };
+
+      btn.onclick = () =>{
       taskList.removeChild(li)
       taskList.removeChild(btn)
       }
